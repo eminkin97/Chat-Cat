ce64b90b9177bafdcf367dcb1320515231cd1d2e
eminkin97 <eminkin99@gmail.com>
2018-01-06 18:35:47 -0500

    Added implementation on server to return list of connected clients

--- chat.go
+++ chat.go
@@ -3,19 +3,34 @@ package main
 import(
 	"net"
 	"log"
+	"bytes"
 )
 
 type client_struct struct {
 	name string
 	conn net.Conn
+	ch chan string
 }
 
 var connectedClients []client_struct
 
+//function returns concatenated list of client names seperated by comma
+func concatenateClientNames() string {
+	var buffer bytes.Buffer
+
+	//append all names of clients to the client_name_list
+	for _, elem := range connectedClients {
+		buffer.WriteString(elem.name)
+		buffer.WriteString(",")
+	}
+
+	return buffer.String()
+}
+
 func handleRequest(c net.Conn) {
 	log.Println("REQUEST HANDLED BRA")
 
-	//read name from connection
+	//read from connection
 	b := make([]byte, 10)
 	n, err := c.Read(b)
 
@@ -23,19 +38,73 @@ func handleRequest(c net.Conn) {
 		log.Fatal(err)
 	}
 
+	//client logging in
 	name := string(b[:n])
 
+	//create channel for hat with other users
+	ch := make(chan string)
+
 	//create struct for client
-	new_client := client_struct{name: name, conn: c}
+	new_client := client_struct{name: name, conn: c, ch: ch}
 
 	//append new client to connectedClients
 	connectedClients = append(connectedClients, new_client)
+
+	//client is done once he/she sends exit message
+	for {
+
+		//check to see if received a chat request from other user
+		/*select {
+			case externalRequest := <-c2:
+				log.Println(res)
+			case <-time.After(time.Second * .1):
+				log.Println("no chat request")
+		}*/
+
+		//action to be performed from client
+		a := make([]byte, 20)
+
+		//read message from client
+		n1, err := c.Read(a)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		action := string(a[:4])
+
+		if action == "list" {
+			//write back list of connected clients
+			client_name_list := concatenateClientNames()
+
+			_, err = c.Write([]byte(client_name_list))
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else if action == "chat" {
+			//start chat with name nameToChat
+			nameToChat := string(a[5:n1])
+			log.Println(nameToChat)
+		} else if action == "exit" {
+			//remove name from connected clients then break
+			for i, elem := range connectedClients {
+				if elem.name == name {
+					//found the one to remove
+					connectedClients = append(connectedClients[0:i], connectedClients[i+1:]...)
+					break
+				}
+			}
+			break
+		}
+	}
+
 	log.Println("REQUEST DONE BRA")
 }
 
 func main() {
 	//listen for connections
 	ln, err := net.Listen("tcp", ":8001")
+
+
 	if err != nil {
 		log.Fatal(err)
 	}
--- chat_test.go
+++ chat_test.go
@@ -33,12 +33,6 @@ func TestClientConnect(t *testing.T) {
 		t.Logf("numwritten was not equal to y. Was %d", numWritten)
 	}
 
-	//synchronize via channel
-	<-done
-
-	//this routine acts as client in the test
-	client.Close()
-
 	//check to see if appended local ip address to connected clients slice
 	if len(connectedClients) <= 0 {
 		t.Error("connected Clients is empty")
@@ -48,4 +42,80 @@ func TestClientConnect(t *testing.T) {
 	if (strings.Compare(connectedClients[0].name, "rudolph") != 0) {
 		t.Errorf("Name was not equal to rudolph. Name was: %s", connectedClients[0].name)
 	}
+
+	//write exit action to indicate we're done
+	action := []byte("exit")
+
+	_, err = client.Write(action)
+	if err != nil {
+		t.Error(err)
+	}
+
+	//synchronize via channel
+	<-done
+
+	for _, elem := range connectedClients {
+		if elem.name == "rudolph" {
+			t.Error("NAME WASN'T REMOVED AT END")
+		}
+	}
+
+	//this routine acts as client in the test
+	client.Close()
+}
+
+
+func TestListConnectedClients(t *testing.T) {
+	server, client := net.Pipe()
+	done := make(chan bool, 1)
+
+	go func(c net.Conn, t *testing.T, done chan bool) {
+		//this routine acts as the server in the test
+		handleRequest(c)
+
+		done <- true
+		c.Close()
+		log.Println("MEOW")
+	}(server, t, done)
+
+	//write from client connection to server connection
+	name := []byte("rudolph")
+
+	_, err := client.Write(name)
+	if err != nil {
+		t.Error(err)
+	}
+
+	//write list action to indicate want list of all clients
+	action := []byte("list")
+
+	_, err = client.Write(action)
+	if err != nil {
+		t.Error(err)
+	}
+
+	returned_bytes := make([]byte, 100)
+	n, err := client.Read(returned_bytes)
+
+	client_list := string(returned_bytes[:n])
+
+	if client_list != "rudolph," {
+		t.Error("Client list was wrong")
+	}
+
+	//write exit action to indicate we're done
+	action = []byte("exit")
+
+	_, err = client.Write(action)
+	if err != nil {
+		t.Error(err)
+	}
+
+
+	//synchronize via channel
+	<-done
+
+	//this routine acts as client in the test
+	client.Close()
+
 }
